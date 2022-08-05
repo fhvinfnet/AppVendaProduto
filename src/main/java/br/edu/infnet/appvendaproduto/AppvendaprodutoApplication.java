a6fb1bc4e4beb5a0e938940cd4c7054f55690d16
fhvieira <fhvieira@hotmail.com>
2022-08-05 17:55:42 -0300

    first commit

---
+++ src/main/java/br/edu/infnet/appvendaproduto/AppvendaprodutoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appvendaproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppvendaprodutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppvendaprodutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/appvendaproduto/AppvendaprodutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appvendaproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppvendaprodutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
