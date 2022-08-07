9eb922b75462c590e527b18aaeaa4025fd6f4884
fhvieira <fhvieira@hotmail.com>
2022-08-07 18:50:07 -0300

    feature2 classe de teste de cliente

--- src/main/java/br/edu/infnet/appvendaproduto/ClienteTest.java
+++ src/main/java/br/edu/infnet/appvendaproduto/ClienteTest.java
@@ -1,4 +1,25 @@
 package br.edu.infnet.appvendaproduto;
 
-public class ClienteTest {
+import br.edu.infnet.appvendaproduto.model.domain.Cliente;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClienteTest implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        System.out.println("clientes");
+
+        Cliente c1 = new Cliente("12345678901", "joao", "joao@email.com");
+        System.out.println(c1);
+
+        Cliente c2 = new Cliente("12345678902", "maria", "joao@email.com");
+        System.out.println(c2);
+
+        Cliente c3 = new Cliente("12345678903", "jose", "joao@email.com");
+        System.out.println(c3);
+    }
 }
--- src/main/java/br/edu/infnet/appvendaproduto/model/domain/Cliente.java
+++ src/main/java/br/edu/infnet/appvendaproduto/model/domain/Cliente.java
@@ -16,4 +16,28 @@ public class Cliente {
     public String toString() {
         return nome + ";" + cpf + ";" + email;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
